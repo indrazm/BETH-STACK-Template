1525387c5a3ec76a85024c82868ae74779912e9e
Indra Zulfi <me@indrazm.com>
2023-11-28 16:47:55 +0700

    feat: edit dashboard

--- src/components/apps/dashboard/index.tsx
+++ src/components/apps/dashboard/index.tsx
@@ -5,8 +5,11 @@ export const Dashboard = async ({ userId }: { userId: string }) => {
   const user = await auth.getUser(userId);
   return (
     <BaseHTML>
-      <header class="flex justify-between p-4 items-center">
-        <div class="font-bold tracking-tighter text-lg">BETH Stack</div>
+      <header class="flex justify-between p-4 items-center bg-gradient-to-b from-indigo-100 to-transparent">
+        <div class="flex gap-2 items-center">
+          <img src="public/logo.svg" width="24px" height="24px" />
+          <div class="font-bold tracking-tighter text-lg"></div>
+        </div>
         <nav class="flex items-center gap-4">
           <div safe>{user.name}</div>
           <form method="post" action="/logout">
@@ -14,6 +17,25 @@ export const Dashboard = async ({ userId }: { userId: string }) => {
           </form>
         </nav>
       </header>
+      <main class="mt-12 max-w-4xl m-auto space-y-12">
+        <section class="space-y-2">
+          <h4>Here is the dashboard</h4>
+          <p>Feel free to use and edit...</p>
+        </section>
+        <section class="space-y-3">
+          <h5>Tech Stack</h5>
+          <ul>
+            <li>Bun.js</li>
+            <li>Elysiajs</li>
+            <li>Turso</li>
+            <li>HTMX</li>
+          </ul>
+        </section>
+        <section class="space-y-3">
+          <h5>Another tech</h5>
+          <p>It use Lucia Auth for Authentication, Drizzle for Database ORM, Tailwindcss, and Typescripts.</p>
+        </section>
+      </main>
     </BaseHTML>
   );
 };
